83dd4b53b4098948f1cd3814836e88a88de6009f
tolulope <tolulopefaleyejanet@gmail.com>
2023-10-25 16:49:56 +0100

    Airvy responsive2

--- src/Component/BookAppointment.js
+++ src/Component/BookAppointment.js
@@ -9,17 +9,19 @@ const BookAppointment = () => {
   function updateToggle(id){
     setToggle(id)
   }
+
+  
   return (
     <>
       <Header />
 
       <div className="subNav">
         <div className="container text-center mt-5">
-          <h3 className="pt-5 text-uppercase couture-heading">
+          <h3 className="pt-5 text-uppercase couture-heading BA-heading">
             Experience Bridal Couture
           </h3>
-          <h3 className="pt-1 home-heading">Your Appointment Awaits</h3>
-          <p className="home-text pb-5">
+          <h3 className="pt-1 home-heading BA-big-heading">Your Appointment Awaits</h3>
+          <p className="home-text pb-5 BA-text">
             Are you on the journey to finding the perfect wedding gown? We're
             here to help! At Airvy, we offer personalized bridal dress
             consultations to bring your dream wedding look to life. Our expert
@@ -29,7 +31,7 @@ const BookAppointment = () => {
         </div>
  
         <div className="d-flex justify-content-around tabs">
-          <div className="tab" onClick={()=>updateToggle(1)}>
+          <div className="tab active" onClick={()=>updateToggle(1)}>
             Bridal Consultant
           </div>
           <div className="tab" onClick={()=>updateToggle(2)}>
@@ -40,10 +42,10 @@ const BookAppointment = () => {
  
       <div className="container text-center mt-5 ">
       <div className={toggle === 1 ? "show-content" : "content"}>
-        <h3 className="pt-1 home-heading">
+        <h3 className="pt-1 home-heading BA-big-heading2">
           Let's pair you with one of our bridal consultants
         </h3>
-        <p className="home-text">
+        <p className="home-text BA-text">
           Looking for the perfect wedding dress? A personal wedding dress
           consultant is like a valuable investment on your special journey. Our
           mission is to assist and guide you personally, helping you overcome
@@ -64,12 +66,12 @@ const BookAppointment = () => {
 
       <div className="d-flex flex-column justify-content-center">
       <div className="container">
-        <p className="designer-text">
+        <p className="designer-text BA-text">
           Elevate your bridal experience with our exclusive consultation
           option—your chance to work directly with our renowned Top Designer.
           </p>
-          <p className="designer-text">During your consultation, you'll have the opportunity to:</p>
-          <ul className="text-left designer-text">
+          <p className="designer-text BA-text">During your consultation, you'll have the opportunity to:</p>
+          <ul className="text-left designer-text BA-text">
             <li>
             Explore our stunning collection of wedding dresses.
           </li>
@@ -83,7 +85,7 @@ const BookAppointment = () => {
           Get recommendations tailored to your unique vision.
           </li>
           </ul>
-          <p className="pb-5 designer-text">We can't wait to be a part of your bridal journey and help you
+          <p className="pb-5 designer-text BA-text">We can't wait to be a part of your bridal journey and help you
           find the gown of your dreams.</p>
         
       </div>
